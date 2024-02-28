183fa57c9ad30afa5d28481ffd19a78582b03a67
José de los santos <delossantosramosjose@gmail.com>
2024-02-28 15:50:01 +0100

    Base configuration added to astro.config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  base: '/stardew-resume',
 });
