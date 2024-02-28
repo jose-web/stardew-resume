d58545ebb8639ace97a1bbd8072058f8ca3fdf97
José de los santos <delossantosramosjose@gmail.com>
2024-02-28 14:24:39 +0100

    Parse XML to JSON

--- public/file-reader.js
+++ public/file-reader.js
@@ -9,9 +9,48 @@ window.onload = () => {
 function fileUploaded(event) {
     let file = event.target.files[0]
     let reader = new FileReader()
-    reader.readAsText(file);
+    reader.readAsText(file)
 
     reader.onload = () => {
-        console.log(reader.result)
+
+        try {
+            let xmlInfo = new DOMParser().parseFromString(reader.result, 'application/xml')
+            let jsonInfo = xml2json(xmlInfo)
+            let saveGame = jsonInfo.SaveGame
+
+            console.log(saveGame);
+            
+        } catch (e) {
+            console.error("Error parsing XML:", e);
+        }
 	}
+}
+
+function xml2json(xml) {
+  try {
+    var obj = {};
+    if (xml.children.length > 0) {
+      for (var i = 0; i < xml.children.length; i++) {
+        var item = xml.children.item(i);
+        var nodeName = item.nodeName;
+
+        if (typeof (obj[nodeName]) == "undefined") {
+          obj[nodeName] = xml2json(item);
+        } else {
+          if (typeof (obj[nodeName].push) == "undefined") {
+            var old = obj[nodeName];
+
+            obj[nodeName] = [];
+            obj[nodeName].push(old);
+          }
+          obj[nodeName].push(xml2json(item));
+        }
+      }
+    } else {
+      obj = xml.textContent;
+    }
+    return obj;
+  } catch (e) {
+      console.log(e.message);
+  }
 }
